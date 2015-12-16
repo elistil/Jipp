d3aecf0c9fc7b2fee5e4eb5a295940648c881dbb
elistil <sabak.kamil@gmail.com>
2015-12-16 22:50:50 +0100

    brakujace pliki

--- main.cpp
+++ main.cpp
@@ -2,8 +2,7 @@
 #include <windows.h>
 #include "headers/vector.h"
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 	Vector *vec = new Vector(1,1,1);
 	std::cout << vec->getX() << std::endl;
 	system("pause");
