f9bc291c5378d82c6c25a2b60079333b73dd1d10
elistil <sabak.kamil@gmail.com>
2015-12-18 14:34:36 +0100

    tabs

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 
 int main(int argc, char* argv[]) {
 	Vector *vec = new Vector(1,1,1);
+	
 	std::cout << vec->getX() << std::endl;
 	std::cout << "Enter to exit the program: ";
 	std::cin.ignore().get();
+	
     return 1;
 }
