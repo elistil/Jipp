7c7854697c5a404cce39198cfe6b968f1388e767
elistil <sabak.kamil@gmail.com>
2015-12-18 14:30:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include <windows.h>
+//#include <windows.h>
 #include "headers/vector.h"
 
 int main(int argc, char* argv[]) {
 	Vector *vec = new Vector(1,1,1);
 	std::cout << vec->getX() << std::endl;
-	system("pause");
+	std::cout << "Enter to exit the program: ";
+	std::cin.ignore().get();
     return 1;
 }
